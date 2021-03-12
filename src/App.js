aedd16cc1fd660002f878397378f6de2c4861166
Try Wijono <3wijono@gmail.com>
2021-03-12 19:35:15 +0800

    add Semantic UI css and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import { shipFactory } from "./factories/shipFactory";
-import gameBoardFactory from "./factories/gameBoardFactory";
+import { gameBoardFactory } from "./factories/gameBoardFactory";
+import { Link } from "react-router-dom";
+import { Header } from "semantic-ui-react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header as="h1" textAlign="center">
+        Battleship Game
+      </Header>
+      <Header as="h2" textAlign="center">
+        <Link to="/">Start Game</Link>
+      </Header>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { HashRouter, Switch } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Switch>
+        <App />
+      </Switch>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
