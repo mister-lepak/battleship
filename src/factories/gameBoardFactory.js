98e7526c10f45fd9d6383b0c90db9a3f95a12586
Try Wijono <3wijono@gmail.com>
2021-03-14 16:27:11 +0800

    add functionalities in the initialization

--- src/factories/gameBoardFactory.js
+++ src/factories/gameBoardFactory.js
@@ -1,9 +1,9 @@
-// import shipFactory from "./shipFactory";
+import shipFactory from "./shipFactory";
 import _ from "lodash";
 import { Grid, Segment } from "semantic-ui-react";
 
 const gameBoardFactory = (shipTiles, boardTiles = []) => {
-  const initialize = () => {
+  const initialize = (player = "user") => {
     const tilesWithShips = [];
     _.forOwn(shipTiles, function (coordinatesSets, key) {
       _.forEach(coordinatesSets, function (coordinates) {
@@ -14,16 +14,27 @@ const gameBoardFactory = (shipTiles, boardTiles = []) => {
     return _.times(100, (i) => {
       let hasShipValue = false;
       let gridClass = "eachGrid";
-      if (tilesWithShips.includes(i)) {
-        hasShipValue = true;
-        gridClass = "shipGrid";
-      }
+      if (tilesWithShips.includes(i)) hasShipValue = true;
+      if (tilesWithShips.includes(i) && player !== "AI") gridClass = "shipGrid";
+
       boardTiles.push({ hasShip: hasShipValue, isShot: false });
       return (
         <Grid.Column
           textAlign="center"
           key={i}
           className={gridClass}
+          onClick={(e) => {
+            if (!receiveAttack(i)) {
+              e.target.classList.remove("eachGrid");
+              e.target.classList.add("missedShotGrid");
+            } else {
+              e.target.classList.remove("shipGrid");
+              e.target.classList.add("damagedGrid");
+            }
+            if (areAllShipsSunk()) {
+              console.log("all ships sunk!");
+            }
+          }}
         ></Grid.Column>
       );
     });
@@ -53,14 +64,16 @@ const gameBoardFactory = (shipTiles, boardTiles = []) => {
     boardTiles[sequenceCalc].hasShip = true;
   };
 
-  const receiveAttack = (coordinates) => {
-    const sequenceCalc = coordinates[0] * 10 + coordinates[1];
-    if (boardTiles[sequenceCalc].isShot) return false;
+  const receiveAttack = (sequenceCalc) => {
+    // const sequenceCalc = coordinates[0] * 10 + coordinates[1];
+    if (boardTiles[sequenceCalc].isShot) console.log("this tile has been hit!");
+    boardTiles[sequenceCalc].isShot = true;
     if (boardTiles[sequenceCalc].hasShip) {
       // shipFactory.hit([coordinates]);
-      boardTiles[sequenceCalc].isShot = true;
+      return true;
     }
-    return true;
+    console.log(boardTiles[sequenceCalc]);
+    return false;
   };
 
   const isAlreadyAttacked = (coordinates) => {
@@ -77,17 +90,18 @@ const gameBoardFactory = (shipTiles, boardTiles = []) => {
     if (tilesWithShips === undefined || tilesWithShips.length === 0)
       return false;
     const areAllShipsShot = tilesWithShips.reduce(
-      (cumulative, current) => cumulative.isShot * current.isShot,
-      []
+      (cumulative, current) => cumulative && current.isShot,
+      true
     );
+    console.log(areAllShipsShot);
     if (!areAllShipsShot) return false;
-    return tilesWithShips;
+    return true;
   };
 
   return {
     initialize,
     placeShipOnBoard,
-    // shipFactory,
+    shipFactory,
     receiveAttack,
     isAlreadyAttacked,
     areAllShipsSunk,
