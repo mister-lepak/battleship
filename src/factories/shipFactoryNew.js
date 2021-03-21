2a17d01bd5c29f38e9987f7368f2dac7c05352a5
Try Wijono <3wijono@gmail.com>
2021-03-21 22:45:12 +0800

    fix shipFactory Test and code fix on the shipFactory code

--- src/factories/shipFactoryNew.js
+++ src/factories/shipFactoryNew.js
@@ -1,11 +1,16 @@
 import _ from "lodash";
 
 const shipFactory = (boundary) => {
-  const shipDamagePosition = [];
+  let shipDamagePosition = [];
 
-  _.times(boundary.length, (i) => {
-    shipDamagePosition.push(false);
-  });
+  const setShipInitialStatus = (value) => {
+    shipDamagePosition = [];
+    return _.times(boundary.length, (i) => {
+      shipDamagePosition.push(value);
+    });
+  };
+
+  setShipInitialStatus(false);
 
   const isSunk = () => {
     return shipDamagePosition.reduce(
@@ -17,24 +22,35 @@ const shipFactory = (boundary) => {
   const hit = (x, y) => {
     const shipCoordinateObj = { x, y };
     let axis = "x";
+    let antiAxis = "y";
     const isInsideShipBoundary = () => {
-      if (boundary.orientation === "horizontal") axis = "x";
-      if (boundary.orientation === "vertical") axis = "y";
-      return shipCoordinateObj[axis] < boundary.length;
+      if (boundary.orientation === "horizontal") {
+        axis = "x";
+        antiAxis = "y";
+      }
+      if (boundary.orientation === "vertical") {
+        axis = "y";
+        antiAxis = "x";
+      }
+      return (
+        shipCoordinateObj[axis] >= 0 &&
+        shipCoordinateObj[axis] < boundary.length &&
+        shipCoordinateObj[antiAxis] === 0
+      );
     };
     const isAttacked = () => {
       const shipPosition = shipCoordinateObj[axis];
       return shipDamagePosition[shipPosition];
     };
 
-    if (isInsideShipBoundary && !isAttacked) {
+    if (isInsideShipBoundary() && !isAttacked()) {
       isSunk();
       return true;
     }
     return false;
   };
 
-  return { shipDamagePosition, hit, boundary };
+  return { shipDamagePosition, hit, boundary, isSunk, setShipInitialStatus };
 };
 
 module.exports = { shipFactory };
